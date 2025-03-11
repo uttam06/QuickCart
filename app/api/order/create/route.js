38378a073bd97c2093670a5ba9f49190734c1dcf
uttam06 <dahaluttam06@gmail.com>
2025-03-11 18:59:23 +0545

    minor changes

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -18,7 +18,7 @@ export async function POST(request) {
     //Calculate amount using items
     const amount = await items.reduce(async (acc, item) => {
       const product = await Product.findById(item.product);
-      return acc + product.offerPrice * item.quantity;
+      return await acc + product.offerPrice * item.quantity;
     }, 0)
 
     await inngest.send({
